344933d53433e81ea33c5fa9e61dd91e8153ed75
LoanMai9 <loanmai.bb99@gmail.com>
2021-03-07 10:03:28 +0700

    .

--- SQL/Assignment/TestingSystem3.sql
+++ SQL/Assignment/TestingSystem3.sql
@@ -1,22 +1,4 @@
--- thông tin các account tham gia vào bao nhiêu group
-        -- b1 thống kê các account tham gia vào bao nhiêu group
-        -- account số lượng
-        SELECT *, COUNT(GroupID) as sluong
-        FROM groupaccount
-        GROUP BY AccountID;
-        
-        -- B2 lấy max số lượng (2)
-        SELECT *, COUNT(GroupID) as sluong
-        FROM groupaccount
-        GROUP BY AccountID
-        ORDER BY sluong DESC
-        LIMIT 1;
-        -- b3 : lấy số  lương = max
-        SELECT AccountID, COUNT(GroupID) as sluong
-        FROM groupaccount
-        GROUP BY AccountID
-        HAVING sluong=3;
-        
+
 -- Question 1: Tạo view có chứa danh sách nhân viên thuộc phòng ban sale
 CREATE VIEW dsnhanviensale AS
 ( SELECT *
@@ -25,7 +7,7 @@ WHERE DepartmentID = (	SELECT DepartmentID
 						FROM Department 
                         WHERE DepartmentName =N'Sale'));
                         
---  thông tin các account tham gia vào nhiều group nhất
+-- Question 2 thông tin các account tham gia vào nhiều group nhất
 
 CREATE VIEW acctgianhieugroup AS (
      SELECT AccountID, COUNT(GroupID) as sluong
@@ -40,19 +22,46 @@ CREATE VIEW acctgianhieugroup AS (
           -- Dạng CTE 
 	WITH acctgianhieugroup AS (
 			SELECT AccountID, COUNT(GroupID) as sluong
-			FROM groupaccount
+			FROM GroupAccount
 			GROUP BY AccountID)
         SELECT *
         FROM acctgianhieugroup 
-        WHERE sluong = (SELECT MAX(sluong) FROM acctgianhieugroup
-						)
+        WHERE sluong = (SELECT MAX(sluong) 
+						FROM acctgianhieugroup)
                         ;
                         
 -- Question 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 300 từ được coi là quá dài) và xóa nó đi
+CREATE OR REPLACE VIEW vw_ContenTren18Tu
+AS
+SELECT 	LENGTH(Content)
+FROM	Question
+WHERE	LENGTH(Content) > 18;
+SELECT * FROM vw_ContenTren18Tu;
+DROP VIEW vw_ContenTren18Tu;
+
 
 -- Question 4: Tạo view có chứa danh sách các phòng ban department có nhiều nhân viên account nhất
- SELECT *, COUNT(DepartmentID)
- FROM `Account`
+ CREATE OR REPLACE VIEW DepartmentMaxAccount AS
+SELECT 		D.*, COUNT(A.DepartmentID)
+FROM		Department D 
+JOIN `Account` A ON D.DepartmentID = A.DepartmentID
+GROUP BY	D.DepartmentID
+HAVING		COUNT(A.DepartmentID) = (
+									SELECT 		COUNT(A.DepartmentID)
+									FROM		Department D 
+                                    JOIN `Account` A ON	D.DepartmentID = A.DepartmentID
+									GROUP BY	D.DepartmentID
+									HAVING		COUNT(A.DepartmentID)
+                                    ORDER BY	COUNT(A.DepartmentID) DESC
+                                    LIMIT		1
+                                    );
+SELECT * FROM DepartmentMaxAccount;
 
 
 -- Question 5: Tạo view có chứa tất các các câu hỏi do user họ Nguyễn tạo
+CREATE OR REPLACE VIEW vw_QuesHoNguyen
+AS
+SELECT 		Q.*, A.FullName
+FROM 		Question Q
+ JOIN `Account` A ON Q.CreatorID = A.AccountID
+WHERE		SUBSTRING_INDEX(FullName,' ',1) = 'Nguyen';
