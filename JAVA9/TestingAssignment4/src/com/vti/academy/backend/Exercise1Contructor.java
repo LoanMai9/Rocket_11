6d40d9f6a22c2553733d86dd7984e47b49f2d5d2
LoanMai9 <loanmai.bb99@gmail.com>
2021-03-26 16:28:09 +0700

    Submit

--- JAVA9/TestingAssignment4/src/com/vti/academy/backend/Exercise1Contructor.java
+++ JAVA9/TestingAssignment4/src/com/vti/academy/backend/Exercise1Contructor.java
@@ -1,16 +1,36 @@
 package com.vti.academy.backend;
 
+import java.time.LocalDate;
+
+import com.vti.academy.entity.Account;
 import com.vti.academy.entity.Department;
+import com.vti.academy.entity.Position;
 
 public class Exercise1Contructor {
 
-	// ques 1
-		public void ques1() {
-			float salary1 = 5240.5f;
-			float salary2 = 10970.065f;
-			int a = (int) salary1;
-			int b = (int) salary2;
-			System.out.println(b);
-		}
+	public void question1() {
 
+		// construct object
+		Department department = new Department();
+		Department department1 = new Department("Sale");
+
+		// print object
+		System.out.println(department);
+		System.out.println(department1);
+	}
+	public void question2() {
+		// a
+		Account account = new Account();
+		System.out.println(account);
+
+		// b
+		Account account1 = new Account(1, "abc@gmail.com", "abc", "sauds", "sadsda");
+		System.out.println(account1);
+
+		// c
+		Position position1 = new Position(1, "Sale");
+		Position position2 = new Position(2, "Marketing");
+				LocalDate.parse("2020-05-15");
+		System.out.println(account1);
+	}
 }
