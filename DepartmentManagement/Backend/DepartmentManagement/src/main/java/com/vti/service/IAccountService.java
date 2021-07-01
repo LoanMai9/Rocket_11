4bdaccfbb83255e601ea3ee7a979c3fd73d7cefa
LoanMai9 <loanmai.bb99@gmail.com>
2021-07-01 20:29:29 +0700

    submit

---
+++ DepartmentManagement/Backend/DepartmentManagement/src/main/java/com/vti/service/IAccountService.java
@@ -0,0 +1,11 @@
+package com.vti.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.vti.entity.Account;
+
+public interface IAccountService extends UserDetailsService{
+	Account getAccountByUsername(String username);
+
+
+}
