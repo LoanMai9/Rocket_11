6ab5106906e57a0ccdc944b980a7b22475463b21
LoanMai9 <loanmai.bb99@gmail.com>
2021-09-16 08:37:40 +0700

    [ Commit]

---
+++ JAVA_CORE/TestingSystem1/src/DemoF/DemoGit.java
@@ -0,0 +1,13 @@
+package DemoF;
+
+import java.util.Iterator;
+
+public class DemoGit {
+	int data[] = { 0, 2, 4};
+	public void display() {
+		System.out.println(" Data: ");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(" Next value: " + data[i]);
+		}
+	}
+}
